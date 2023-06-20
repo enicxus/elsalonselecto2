481f3bdcaf978b9979feddf436ae08ba974e91a4
MatiasBSC <mati.sandoval@duocuc.cl>
2023-06-20 17:46:41 -0400

    Inicio sesion

--- SalonSelecto/urls.py
+++ SalonSelecto/urls.py
@@ -24,7 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
-    path('api/', include('rest_direccion.urls')),
+
+ 
     
 ###################################################
 #vistas formularios
@@ -36,3 +37,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
+
+#path('api/', include('rest_direccion.urls')),
+   
