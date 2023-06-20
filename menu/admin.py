e33411c7e4aa1e0cb18ba5b70d869bac6289cb51
enicxus <tineoanderson7@gmail.com>
2023-06-20 17:01:06 -0400

    q

--- menu/admin.py
+++ menu/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import Pedido, Detalle, Comida, Usuario, Pregunta, Rol, Direccion, Comuna, Region
+from .models import   Comida, Usuario, Tarjeta
 
 # Register your models here.
 
-admin.site.register(Pedido)
-admin.site.register(Detalle)
+admin.site.register(Tarjeta)
 admin.site.register(Comida)
 admin.site.register(Usuario)
-admin.site.register(Pregunta)
-admin.site.register(Rol)
-admin.site.register(Direccion)
-admin.site.register(Comuna)
-admin.site.register(Region)
